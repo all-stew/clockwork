24c1ae1ca00cab8525f2d9378ba414e8de039690
zhaojj11 <zhaojj11@zhaojj11.com>
2022-11-19 02:00:34 +0800

    feat: :green_heart: 优化codecov的ci

--- clockwork-api/src/test/java/com/zhaojj11/clockwork/service/impl/UserServiceImplTest.java
+++ clockwork-api/src/test/java/com/zhaojj11/clockwork/service/impl/UserServiceImplTest.java
@@ -3,28 +3,43 @@ package com.zhaojj11.clockwork.service.impl;
 import com.zhaojj11.clockwork.domain.dao.UserDao;
 import com.zhaojj11.clockwork.domain.model.User;
 import com.zhaojj11.clockwork.service.UserService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
-@ActiveProfiles(value = "test")
+@RunWith(SpringRunner.class)
 class UserServiceImplTest {
 
+    private static final UserService userService = new UserServiceImpl(null);
     @MockBean
-    private UserDao userDao;
+    private static UserDao userDao;
 
-    @Autowired
-    private UserService userService;
+    @BeforeAll
+    static void init() throws Exception {
+        userDao = (UserDao) initBean();
+    }
+
+    // TODO 逻辑抽离出去 并且Mock脱离数据库+不启动Spring+优化测试速度+不引入项目组件
+    static Object initBean() throws Exception {
+        Class<?> clazzRoot = ((Object) UserServiceImplTest.userService).getClass();
+        Object memberFieldObj = Mockito.mock((Class<?>) UserDao.class);
+        String simpleName = UserDao.class.getSimpleName();
+        Field field = clazzRoot.getDeclaredField(simpleName.substring(0, 1).toLowerCase() + simpleName.substring(1));
+        field.setAccessible(true);
+        field.set(UserServiceImplTest.userService, memberFieldObj);
+        return memberFieldObj;
+    }
+
 
     @Test
     void save() {
@@ -36,7 +51,7 @@ class UserServiceImplTest {
 
     @Test
     void remove() {
-        Mockito.when(userDao.removeById(Mockito.anyLong())).thenReturn(true);
+        Mockito.when(userService.remove(Mockito.anyLong())).thenReturn(true);
         boolean removed = userService.remove(6L);
         assertTrue(removed);
     }
@@ -44,7 +59,7 @@ class UserServiceImplTest {
     @Test
     void updateById() {
         User user = User.builder().id(1L).username("test").email("test@test.com").status(User.UserStatus.ENABLE).password("").deleted(false).createdTime(LocalDateTime.now()).updatedTime(LocalDateTime.now()).build();
-        Mockito.when(userDao.updateById(ArgumentMatchers.any(User.class))).thenReturn(true);
+        Mockito.when(userService.updateById(ArgumentMatchers.any(User.class))).thenReturn(true);
         boolean updated = userService.updateById(user);
         assertNotNull(user);
         assertTrue(updated);
@@ -53,7 +68,7 @@ class UserServiceImplTest {
     @Test
     void getById() {
         User userData = User.builder().id(1L).username("test").email("test@test.com").status(User.UserStatus.ENABLE).password("").deleted(false).createdTime(LocalDateTime.now()).updatedTime(LocalDateTime.now()).build();
-        Mockito.when(userDao.getById(Mockito.anyLong())).thenReturn(userData);
+        Mockito.when(userService.getById(Mockito.anyLong())).thenReturn(userData);
         User user = userService.getById(6);
         assertNotNull(user);
     }
