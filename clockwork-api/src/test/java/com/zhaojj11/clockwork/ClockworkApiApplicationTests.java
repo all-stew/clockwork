1285fe9a624dc58b2f89151918251be44c77dd96
zhaojj11 <zhaojj11@zhaojj11.com>
2022-11-19 02:09:07 +0800

    feat: :green_heart: 优化codecov的ci

--- clockwork-api/src/test/java/com/zhaojj11/clockwork/ClockworkApiApplicationTests.java
+++ clockwork-api/src/test/java/com/zhaojj11/clockwork/ClockworkApiApplicationTests.java
@@ -1,9 +1,7 @@
 package com.zhaojj11.clockwork;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ClockworkApiApplicationTests {
 
     @Test
