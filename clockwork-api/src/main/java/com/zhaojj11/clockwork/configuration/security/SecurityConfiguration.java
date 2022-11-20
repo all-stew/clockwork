61ff0422af430e0bf859f10d43797ef214eb49c5
zhaojj11 <zhaojj11@zhaojj11.com>
2022-11-20 23:20:57 +0800

    feat: :sparkles: 引入spring-security

---
+++ clockwork-api/src/main/java/com/zhaojj11/clockwork/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.zhaojj11.clockwork.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * @author zhaojj11
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
+        return http.build();
+    }
+}
